e3630267e22fe209ac7700b5befff10c52b774e5
Sebastian Marciales <Sebastianmarciales40@gmail.com>
2025-08-29 16:20:03 -0500

    Add images

--- src/app/pages/services-section/services-section.component.ts
+++ src/app/pages/services-section/services-section.component.ts
@@ -15,13 +15,13 @@ type ServiceKey =
 
 export class ServicesSectionComponent {
   items: Array<{key: ServiceKey; icon: string}> = [
-    { key: 'digital',      icon: 'images/digital.png' },
-    { key: 'whitening',    icon: '/assets/images/icons/whitening.png' },
-    { key: 'veneers',      icon: '/assets/images/icons/veneers.png' },
-    { key: 'cleaning',     icon: '/assets/images/icons/cleaning.png' },
-    { key: 'jewelry',      icon: '/assets/images/icons/dental-jewelry.png' },
-    { key: 'smileDesign',  icon: '/assets/images/icons/smile-design.png' },
-    { key: 'rehabImplant', icon: '/assets/images/icons/rehab-implant.png' },
-    { key: 'prosthesis',   icon: '/assets/images/icons/prosthesis.png' }
+    { key: 'digital',      icon: 'images/digital.jpg' },
+    { key: 'whitening',    icon: 'images/blanqueamiento.jpeg' },
+    { key: 'veneers',      icon: 'images/carillas.jpeg' },
+    { key: 'cleaning',     icon: 'images/blanqueamiento2.jpeg' },
+    { key: 'jewelry',      icon: 'images/joyas.jpg' },
+    { key: 'smileDesign',  icon: 'images/diseno.jpeg' },
+    { key: 'rehabImplant', icon: 'images/protesis.jpeg' },
+    { key: 'prosthesis',   icon: 'images/protesis1.jpeg' }
   ];
 }
